b74d85f3c9bbde6389b9a53bc572a62b11ad7c60
dean thurston <deanthurston2791@gmail.com>
2017-03-07 18:04:44 -0500

    initial commit

---
+++ Assets/Extensions.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ExtensionMethods
+{
+    public static Vector2 XZ(this Vector3 vec)
+    {
+        return new Vector2(vec.x, vec.z);
+    }
+
+    public static Vector2 XY(this Vector3 vec)
+    {
+    	return new Vector2(vec.x, vec.y);
+    }
+
+    public static Vector3 XYZ(this Vector2 vec, float z)
+    {
+    	return new Vector3(vec.x, vec.y, z);
+    } 
+	
+    public static Vector3 XSetYZ(this Vector2 vec, float y)
+    {
+        return new Vector3(vec.x, y, vec.y);  
+    }
+	public static float DistanceSqr(this Vector2 p1, Vector2 p2)
+	{
+		float distX = p1.x - p2.x;
+		float distY = p1.y - p2.y;
+
+		return (distX * distX) + (distY * distY);
+	}
+
+    public static Vector3 OrthoNormalize(Vector3 vector1, Vector3 vector2)
+    {
+        vector1.Normalize();
+        Vector3 temp = Vector3.Cross(vector1, vector2);
+        temp.Normalize();
+        vector2 = Vector3.Cross(temp, vector2);
+        return vector2;
+        //var output = new Vector3[] {vector1, vector2};
+        //return output;
+    }
+
+
+    public static void AddTorqueAtPosition(this Rigidbody2D thisRigidbody, Vector3 torque, Vector3 position)
+    {
+        //http://forum.unity3d.com/threads/torque-at-offset.187297/
+        Vector3 torqueAxis = torque.normalized;
+        Vector3 ortho = new Vector3(1, 0, 0);
+        // prevent torqueAxis and ortho from pointing in the same direction
+        if ((torqueAxis - ortho).sqrMagnitude < float.Epsilon)
+        {
+            ortho = new Vector3(0, 1, 0);
+        }
+
+        var orthoNorm = ExtensionMethods.OrthoNormalize(torqueAxis, ortho);
+
+
+        // calculate force
+        Vector3 force = Vector3.Cross(0.5f * torque, orthoNorm);
+        thisRigidbody.AddForceAtPosition(force, position + orthoNorm);
+        thisRigidbody.AddForceAtPosition(-force, position - orthoNorm);
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Vector2 movement;
+    public float movementForce;
+    public float jumpTime;
+    public float jumpForce;
+    public float spinForce;
+    public float getUpForce;
+    public float wallJumpForce;
+    public Transform getUpPosition;
+    Rigidbody2D rb;
+    float startJumpTime;
+    public bool onGround;
+    bool jumping;
+    List<Transform> corners;
+    List<Transform> collisions;
+    int wallJump;
+    // Use this for initialization
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        getUpPosition = transform.GetChild(0);
+        corners = new List<Transform>();
+        for(int i  = 1; i < transform.childCount; i++)
+        {
+            corners.Add(transform.GetChild(i));
+        }
+    }
+
+        
+    // Update is called once per frame
+    void Update()
+    {
+        DetectCollisions();
+        movement = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        if(Input.GetButtonDown("Jump") && onGround)
+        {
+            startJumpTime = Time.time;
+            jumping = true;
+        }
+        if (Input.GetButtonUp("Jump"))
+            jumping = false;
+
+       
+        if (onGround && !Input.GetButton("Turn"))
+        {          
+                rb.AddForceAtPosition(movement * movementForce * Time.deltaTime, transform.position.XY());
+        }            
+        else
+        {
+            // rb.AddTorque(movement.x * -Time.deltaTime * spinForce);
+            float force = getUpForce;
+            if (!onGround)
+                force = spinForce;
+
+            if (Input.GetButton("Turn"))
+            {
+                Vector2 pushPosition1 = (getUpPosition.position.XY() - transform.position.XY()) * Mathf.Abs(movement.x) + transform.position.XY();
+                Vector2 pushPosition2 = transform.position + -transform.up * getUpPosition.localPosition.y * Mathf.Abs(movement.x);
+                rb.AddForceAtPosition(movement.x * transform.right * force * Time.deltaTime, pushPosition1);
+                rb.AddForceAtPosition(-movement.x * transform.right * force * Time.deltaTime, pushPosition2);
+            }
+        }
+
+        if (jumping && Time.time < startJumpTime + jumpTime)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce * Time.deltaTime);
+        }
+
+        if(wallJump != 0 && Input.GetButtonDown("Jump") && !onGround)
+        {
+            rb.velocity = Vector2.zero;
+            Vector2 jumpVector = transform.right.XY() * wallJump + new Vector2(0, 1.5f);
+            rb.AddForce(jumpVector * wallJumpForce);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+     /*   Vector2 pos = Vector2.zero;
+        int num = 0;
+        foreach (ContactPoint2D con in col.contacts)
+        {
+            pos += con.point;
+            num++;
+        }
+        pos /= num;
+
+        if (pos.y < transform.position.y)
+            onGround = true;*/
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+     /*   Vector2 pos = Vector2.zero;
+        int num = 0;
+        foreach (ContactPoint2D con in col.contacts)
+        {
+            pos += con.point;
+            num++;
+        }
+        pos /= num;
+
+        if (pos.y < transform.position.y)
+            onGround = false;*/
+    }
+
+    void DetectCollisions()
+    {
+        collisions = new List<Transform>();
+        foreach (Transform t in corners)
+        {
+            Collider2D col = Physics2D.OverlapPoint(t.position);
+
+            if (col != null)
+            { collisions.Add(t);  }
+        }
+        int below = 0;
+        int left = 0;
+        int right = 0;
+        if (collisions.Count > 1)
+        {          
+            foreach (Transform t in collisions)
+            {
+                if (t.position.y < transform.position.y)
+                    below++;
+                if (t.position.x < transform.position.x)
+                    left++;
+                else
+                    right++;
+            }
+        }
+        if (left > 1)
+            wallJump = 1;
+        else
+        {
+            if (right > 1)
+                wallJump = -1;
+            else
+                wallJump = 0;
+        }
+
+        if (below > 1)
+            onGround = true;
+        else
+            onGround = false;
+    }
+}
